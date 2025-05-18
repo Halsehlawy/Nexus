abf39a51ff10e06b9c5db2d75fe492cbe0971e7c
halsehlawy <halsehlawi@outlook.com>
2025-05-18 22:08:25 +0300

    NetGraph

--- src/ui/pages/network/networkSecurity.tsx
+++ src/ui/pages/network/networkSecurity.tsx
@@ -4,7 +4,7 @@ import StatCard from "../../components/StatCard"
 import StatGrid from "../../components/StatGrid"
 import { BrickWallFire, Search, ArrowUpDown, Siren } from "lucide-react"
 import "../../styles/NetworkSecurity.css"
- 
+import netGraph from "../../assets/netGraph.png"
 
 const NetworkSecurity = () => {
   const navigate = useNavigate()
@@ -13,10 +13,12 @@ const NetworkSecurity = () => {
     <Layout title="Network Security">
       <StatGrid>
         <StatCard icon={<BrickWallFire size={30} />} label="Firewall" onClick={() => navigate("/network/firewallRules")} />
-        <StatCard icon={<Search size={30} />} label="Network Scan" onClick={() => navigate("/network/networkScan")}/>
-        <StatCard icon={<ArrowUpDown size={30} />} label="Traffic " onClick={() => navigate("/network/traffic")}/>
-        <StatCard icon={<Siren size={30} />} label="Rogue Devices" onClick={() => navigate("/network/rogueDevices")}/></StatGrid>
-    </Layout>
+        <StatCard icon={<Search size={30} />} label="Network Scan" onClick={() => navigate("/network/networkScan")} />
+        <StatCard icon={<ArrowUpDown size={30} />} label="Traffic " onClick={() => navigate("/network/traffic")} />
+        <StatCard icon={<Siren size={30} />} label="Rogue Devices" onClick={() => navigate("/network/rogueDevices")} />
+      </StatGrid>
+
+<img src={netGraph} alt="Network Traffic Graph" className="network-graph" />    </Layout>
   )
 }
 
